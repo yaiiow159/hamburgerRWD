ff648e543e9788c5871b75a64ecfbf5db7003565
Timmy <examyou076@gmail.com>
2020-09-15 11:05:30 +0800

    total  chage
    a lot of change by setting

--- js/burgers.js
+++ js/burgers.js
@@ -18,4 +18,25 @@ $(document).ready(function () {
     $('.top-item').click(function (e) { 
         $('html,body').animate({scrollTop:0},850);
     });
+
+    $('.cart-item button').click(function (e) { 
+       
+        $(this).text('已加入購物車');
+       
+        if (this.click){     
+            var n=$('shop-count a span').text();
+            var m=Number(n);
+            m=m+1;
+            var c=m.toString();
+             $('.shop-count a span').text(c);
+        }
+    });
+    //讓選單秀出來
+    $('.showmenu').click(function (e) { 
+        e.preventDefault();
+        $('body').toggleClass('menu-show');
+    });
+    $('.side-list li').click(function (e) { 
+        e.preventDefault();
+    });
 });
