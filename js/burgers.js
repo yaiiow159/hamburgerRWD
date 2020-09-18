04abc1dc140a3b24abf766ec87143c79c4a6f103
Timmy <examyou076@gmail.com>
2020-09-18 17:05:13 +0800

    jq scrollTop
    use class scrollTop to scroll all the navbar li to the  id part

--- js/burgers.js
+++ js/burgers.js
@@ -1,42 +1,32 @@
 $(document).ready(function () {
-    $('.menu-feature').click(function (e) { 
+
+    $('.scrollTop').click(function (e) {
         e.preventDefault();
-        $('html,body').animate({scrollTop:$('#feature').offset().top},850 );
-    });
-    $('.menu-chef').click(function (e) { 
-        e.preventDefault();
-        $('html,body').animate({scrollTop:$('#chef').offset().top},850,);
-    });
-    $('.menu-map').click(function (e) { 
-        e.preventDefault();
-        $('html,body').animate({scrollTop:$('#map').offset().top},850 );
-    });
-    $('.menu-comment').click(function (e) { 
-        e.preventDefault();
-        $('html,body').animate({scrollTop:$('#comment').offset().top},850);
-    });
-    $('.top-item').click(function (e) { 
-        $('html,body').animate({scrollTop:0},850);
+        var target = $(this).attr('href');
+        var targetPos = $(target).offset().top;
+        
+        $('html,body').animate({scrollTop : targetPos}, 1500);
     });
 
-    $('.cart-item button').click(function (e) { 
-       
+
+    $('.cart-item button').click(function (e) {
+
         $(this).text('已加入購物車');
-       
-        if (this.click){     
-            var n=$('shop-count a span').text();
-            var m=Number(n);
-            m=m+1;
-            var c=m.toString();
-             $('.shop-count a span').text(c);
+
+        if (this.click) {
+            var n = $('shop-count a span').text();
+            var m = Number(n);
+            m = m + 1;
+            var c = m.toString();
+            $('.shop-count a span').text(c);
         }
     });
     //讓選單秀出來
-    $('.showmenu').click(function (e) { 
+    $('.showmenu').click(function (e) {
         e.preventDefault();
         $('body').toggleClass('menu-show');
     });
-    $('.side-list li').click(function (e) { 
+    $('.side-list li').click(function (e) {
         e.preventDefault();
     });
 });
